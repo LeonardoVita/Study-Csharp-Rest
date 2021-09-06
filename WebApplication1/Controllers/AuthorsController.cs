96cb3ffc437f8e1dd9370ba324407a9e91da372c
Leonardo Vita Milanez de Almeida <leonardo.vita0606@gmail.com>
2021-09-06 11:48:46 -0300

    first endpoint

---
+++ WebApplication1/Controllers/AuthorsController.cs
@@ -0,0 +1,28 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet("api/authors")]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
