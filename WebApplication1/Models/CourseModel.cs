a65956931b6ce1ce15470362f6e791a81365bd44
Leonardo Vita Milanez de Almeida <leonardo.vita0606@gmail.com>
2021-09-06 16:58:49 -0300

    courses

---
+++ WebApplication1/Models/CourseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CourseModel
+    {
+        
+        public Guid Id { get; set; }
+
+        
+        public string Title { get; set; }
+
+        
+        public string Description { get; set; }
+
+        public Guid AuthorId { get; set; }
+    }
+}
---
+++ WebApplication1/Profiles/CoursesProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Profiles
+{
+    public class CoursesProfile : Profile
+    {
+        public CoursesProfile()
+        {
+            CreateMap<Course,CourseModel>();
+        }
+    }
+}
