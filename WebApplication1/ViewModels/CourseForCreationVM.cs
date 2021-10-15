2007fc5c494f95a6f7a10dd17e789d64479a6ce2
Leonardo Vita Milanez de Almeida <leonardo.vita0606@gmail.com>
2021-10-14 21:37:23 -0300

    Validation with System.ComponentModel.DataAnnotations and ValidatableObject for custom validate

--- WebApplication1/ViewModels/CourseForCreationVM.cs
+++ WebApplication1/ViewModels/CourseForCreationVM.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApplication1.ViewModels
 {
-    public class CourseForCreationVM
+    public class CourseForCreationVM : IValidatableObject
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [MaxLength(1500)]
         public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Title == Description)
+            {
+                yield return new ValidationResult("The provide description should be different from the title.",
+                    new[] { "CourseForCreationVM" });
+            }
+        }
     }
 }
